b573986c5a4c1dea9be5aacf61265cf66590b125
Jennifer Leonard <jleonard0037x2@hotmail.com>
2017-02-04 06:25:46 -0800

    Modified according to Rue's wishes

--- resu/submit/GitHubDataTest.py
+++ resu/submit/GitHubDataTest.py
@@ -32,12 +32,14 @@ def main(argv):
                 writeFile.write("\n" + user.location.encode('utf8'))
             else:
                 writeFile.write("\nNone")
+            writeFile.write("\nCommitMessages")
             #writeFile.write("\n"+ str(user.public_repos)) #Repos
             #writeFile.write("\n" + str(user.followers)) #followers
             #writeFile.write("\n" + str(user.following)) #following
             #writeFile.write("\n")
             repos = user.get_repos()
             #get_commit_messages(user.get_repos(), user.name)
+            repocount=0
             for repo in repos:
                 writeFile.write("\n" + str(repo.name))
                 #writeFile.write("\n" + str(repo.forks_count))
@@ -45,12 +47,18 @@ def main(argv):
                 writeFile.write("\n" + repo.description.encode('utf8').replace("\t", " ").replace("\n"," "))
                 #commits = repo.get_commits():
                 writeFile.write("\n")
+                count = 0
                 for commit in repo.get_commits():
+                    count = count + 1
+                    if count > 60:
+                        break
                     if commit.author is not None:
                         if commit.author.name == user.name:
                             writeFile.write("\t" + commit.commit.message.encode('utf8').replace("\t", "").replace("\n",""))
-            
-            break
+                repocount= repocount + 1
+                if repocount > 15:
+                    break
+        break
         #we should only ever hit 1 user, so break 
     writeFile.close()
 
