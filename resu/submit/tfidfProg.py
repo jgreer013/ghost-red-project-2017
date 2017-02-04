f34b3b27d422120310385ae63250ecf4b411693c
RueGupta <arushi.gupta.14@gmail.com>
2017-02-04 14:21:11 -0500

    Sort Algortihm for Applicant

--- resu/submit/tfidfProg.py
+++ resu/submit/tfidfProg.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env
-print('Hello')
+#print('Hello')
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer 
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy import  sparse, io
 import csv
-from resume.py import extractResumeText(filename)
-from GitHubDataTest.py import githubMessages(argv)
-from webNltk.py import getWebpageText(url)
+from resume import extractResumeText
+from GitHubDataTest import githubMessages
+from webNltk import getWebpageText
 
 
 def applicant(resumeFileName,githubUserName):
-  diceFileNames = ['jobListingOH.tsv','jobListingPA.tsv']
+  diceFileNames = ['jobListingOH.tsv']#,'jobListingPA.tsv']
   diceData = []
   for filename in diceFileNames:
     reader = csv.reader(open(filename, 'r'),delimiter = '\t')
@@ -33,34 +33,40 @@ def applicant(resumeFileName,githubUserName):
   #docIDCorpus.append('Hello')
 
   resumeDict = extractResumeText(resumeFileName)
-  if (resumeDict["url"] != None):
-    webPageText = getWebpageText(resumeDict["url"])
+  #print(resumeDict["urls"])
+  #print(resumeDict["email"])
+  if (not (resumeDict["urls"] is None) and resumeDict["urls"] != '' ):
+    webPageText = getWebpageText(resumeDict["urls"][0])
     corpus.append(' '.join(webPageText))
     docIDCorpus.append('W'+resumeFileName)
+  else:
+    corpus.append('')
+    docIDCorpus.append('W'+resumeFileName)
+  #print(corpus[len(corpus)-1])
   readGitOutFile = False
-  if (githubUserName != None):
-    githubMessages([githubUserName,'GitHubFetch.txt'])
+  if (not (githubUserName is  None)):
+    githubMessages(githubUserName, 'GitHubFetch')
     readGitOutFile = True
-  else if (resumeDict['email'] != None):
-    githubMessages([resumeDict['email'],'GitHubFetch.txt'])
+  elif (not (resumeDict['email'] is None)):
+    githubMessages(resumeDict['email'], 'GitHubFetch')
     readGitOutFile = True
   else:
     readGitOutFile = False
   if (readGitOutFile == True):
     gitOutput = ''
-    with open('GitHubFetch.txt') as f:
+    with open('GitHubFetch.tsv') as f:
       for line in f.readlines():
         gitOutput = ' '.join(line.split())
   else:
     gitOutput = ''
-
+  #print('gitOut' + gitOutput)
   corpus.append(resumeDict["text"] + gitOutput)
   docIDCorpus.append('R'+resumeFileName)
   vectorizer = TfidfVectorizer(min_df=1)
   tfidf = vectorizer.fit_transform(corpus)
   #print(tfidf)
   csCoeff =cosine_similarity(tfidf) 
-  #print(csCoeff)
+  print(csCoeff)
   #tfidfFile = open('tfidf.mtx','wb+')
   #tfidfFile.write(tfidf)
   #print('Printing 1')
@@ -71,11 +77,74 @@ def applicant(resumeFileName,githubUserName):
   #io.mmwrite(csCoeffFile, csCoeff)
   #ind0 = docIDCorpus.index('Hello')
   #print(ind0)
-  ind1 = docIDCorpus.index('Chttp://www.dice.com/job/result/10105424/5902642-156?src=19')
-  ind2 =  docIDCorpus.index('Chttp://www.dice.com/job/result/10105424/5900266-600?src=19')
+  #ind1 = docIDCorpus.index('Chttp://www.dice.com/job/result/10105424/5902642-156?src=19')
+  #ind2 =  docIDCorpus.index('Chttp://www.dice.com/job/result/10105424/5900266-600?src=19')
   #print(csCoeff[ind1,ind2])
   #print(corpus[ind1])
   #print('two')
   #print(corpus[ind2])
   #print('Done')
+  #print(csCoeff[-2:][:])
+  print(getSortedMax((csCoeff[len(csCoeff)-1][0:len(csCoeff[0])-1]).tolist(),docIDCorpus[0:len(docIDCorpus)-1]))
+  print(getSortedMax((csCoeff[len(csCoeff)-2][0:len(csCoeff[0])-2]).tolist(),docIDCorpus[0:len(docIDCorpus)-2]))
+def company(resumeFileNames, CompanyDesc, JobDesc):
+  print('Hi')
 
+def getSortedMax(csScoreRow,keys):
+  x1 = 0
+  mydict = {}
+  print(len(keys))
+  print(len(csScoreRow))
+  for key in keys:
+  	mydict[key] = csScoreRow[x1]
+	x1 += 1
+  x=0
+  #print(mydict)
+  #return "hi"
+  returnDict = [[],[]]
+  for key2,csScoreRow2 in sorted(mydict.iteritems(),key=lambda(k,v):(v,k),reverse = True):
+    if ( x< 10):
+      returnDict[0].append(key2)
+      returnDict[1].append(csScoreRow2)
+      x = x + 1
+    else:
+      return returnDict
+"""
+def mergeSort(ls,keys):
+    if len(ls) <= 1:
+        return ls
+    else:
+        mid = len(ls)/2
+        left = mergeSort(ls[:mid])
+        right = mergeSort(ls[mid:])
+        left_pt = 0
+        right_pt = 0
+        length = len(left)+len(right)
+        ret = [0]*length
+        retKeys = [0]*length
+        for x in xrange(len(left)+len(right)):
+            if left_pt == len(left):
+                ret[x] = right[right_pt]
+                retKeys[x] = keys[right_pt]
+                right_pt += 1
+            elif right_pt == len(right):
+                ret[x] = left[left_pt]
+                retKeys[x] = keys[left_pt]
+                left_pt += 1
+            else:
+                l = left[left_pt]
+                keyl = key[left_pt]
+                r = right[right_pt]
+                keyr = key[right_pt]
+                if l <= r:
+                    ret[x] = l
+                    retKeys[x] = keyl
+                    left_pt += 1
+                elif r < l:
+                    ret[x] = r
+                    retKeys[x] = keyr
+                    right_pt += 1
+return [ret,retKeys]
+"""
+
+applicant('BakerFN_Resume.pdf','frazierbaker')
